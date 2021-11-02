f7c241e3398d980ce99413db1ac7e24248482c30
Zelechos <zelechos@hotmail.com>
2021-11-02 00:11:08 -0400

    v1.0.8

--- dev/Js/Main.js
+++ dev/Js/Main.js
@@ -1,24 +1,25 @@
 'use strict'
+window.addEventListener('load', () =>{
+    //Animacion parallax de la imagenes de las ropas 
+    document.addEventListener("mousemove", Parallax);
 
-//Animacion parallax de la imagenes de las ropas 
-document.addEventListener("mousemove", Parallax);
+    function Parallax(Evento){
+        this.querySelectorAll('.layer').forEach(layer=>{
+            const speed = layer.getAttribute('data-speed');
+            const x = (window.innerWidth - Evento.pageX*speed)/85;
+            const y = (window.innerHeight - Evento.pageY*speed)/85;
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+    }
 
-function Parallax(Evento){
-    this.querySelectorAll('.layer').forEach(layer=>{
-        const speed = layer.getAttribute('data-speed');
-        const x = (window.innerWidth - Evento.pageX*speed)/85;
-        const y = (window.innerHeight - Evento.pageY*speed)/85;
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    });
-}
+    //Modulo utilizando jquery para el funcionamiento del toggle
+    $(document).ready(()=>{
+        $('.toggle').click(()=>{
+            $('ul').toggleClass('active');
+        });
 
-//Modulo utilizando jquery para el funcionamiento del toggle
-$(document).ready(()=>{
-    $('.toggle').click(()=>{
-        $('ul').toggleClass('active');
-    });
-
-    $('.logo').click(()=>{
-        $('ul').toggleClass('active');
+        $('.logo').click(()=>{
+            $('ul').toggleClass('active');
+        });
     });
 });
